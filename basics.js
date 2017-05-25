14bad986014c0f2fd522a28b8857be1d0ccd4681
Rae <amanda.gaines@gmail.com>
2017-05-25 01:16:34 -0400

    completed basics practice

--- basics.js
+++ basics.js
@@ -162,5 +162,80 @@ console.log(swapNums([2, 3, 5, 7, 6]));
 
 // reverse the values in a given array
 function reverseArray(array){
-  
+  var left = 0;
+  var right = array.length-1;
+  var temp = array[0];
+  for (var i = 0; i < (array.length/2); i++) {
+    array[left] = array[right];
+    array[right] = temp;
+    temp = array[i+1];
+    left ++;
+    right --;
+  }
+  return array;
 }
+console.log(reverseArray([-3,5,1,3,2,10]));
+
+// insert a new number X at an index Y of a given array
+function insertAt(array, x, y){
+  for (var i=array.length; i > y; i --){
+    array[i] = array[i-1];
+  }
+  array[y] = x;
+  return array;
+}
+console.log(insertAt([1, 3, 5, 7] , 10, 2));
+
+// remove negative numbers in a given array without leaving empty or undefined spaces with a for loop
+function removeNegatives(array){
+  for (var i = 0; i < array.length; i++){
+    if (array[i] < 0){
+      for (var j = i + 1; j < array.length; j ++){
+        array[j-1] = array[j];
+        console.log(array);
+      }
+      array.pop();
+      i --;
+    }
+  }
+  return array;
+}
+console.log(removeNegatives( [0, -1, 2, -3, 4, -5, -6, -7, -2, 6]));
+
+// remove negative numbers in a given array without leaving empty or undefined spaces with a while loop
+function removeNegatives(array){
+  var negatives = true;
+  var i = 0;
+  while (negatives){
+    if (array[i] == array[array.length-1]){
+      negatives = false;
+    }
+    if (array[i] < 0){
+      for (var j = i + 1; j < array.length; j ++){
+        array[j-1] = array[j];
+      }
+      array.pop();
+      i --;
+    }
+    i++;
+  }
+  return array;
+}
+console.log(removeNegatives( [0, -1, 2, -3, 4, -5, -6, -7, -2, 6]));
+
+// refactor removeNegatives
+function removeNegatives(array){
+  var negatives = 0;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] < 0){
+      negatives ++;
+    } else {
+      array[i - negatives] = array[i];
+    }
+  }
+  while (negatives--){
+    array.pop();
+  }
+  return array;
+}
+console.log(removeNegatives( [0, -1, 2, -3, 4, -5, -6, -7, -2, 6]));
